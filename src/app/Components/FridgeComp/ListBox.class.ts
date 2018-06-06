b775e2eedfaad78b4a035ccda04dd12daa75e394
JDizon01 <39786880+JDizon01@users.noreply.github.com>
2018-06-06 09:42:31 -0700

    Add files via upload
    w/ Assignment 4

---
+++ src/app/Components/FridgeComp/ListBox.class.ts
@@ -0,0 +1,21 @@
+import { Ingredient } from '../IngredientComp/Ingredient.class';
+
+export class ListBox {
+
+    confirmedList: Ingredient[] = [];
+    shopList: Ingredient[] = [];
+
+    constructor(shopList: Array<Ingredient>, confirmedList: Array<Ingredient>) {
+        this.shopList = shopList;
+        this.confirmedList = confirmedList;
+    }
+
+    getShoplist() {
+        return this.shopList;
+    }
+
+    getConflist() {
+        return this.confirmedList;
+    }
+
+}
